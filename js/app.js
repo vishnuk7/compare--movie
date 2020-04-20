13d3bd5aeab0330a643d0306621e24f79ee5db5f
viu <vishnu.kraman88@gmail.com>
2020-04-20 18:20:39 +0530

    .

--- js/app.js
+++ js/app.js
@@ -2,15 +2,21 @@ import axios from "axios";
 
 // dotenv.config({ path: __dirname + "/.env" });
 console.log(process.env.API_KEY);
-const fetchData = async () => {
+const fetchData = async (movieName) => {
   const res = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: process.env.API_KEY,
-      s: "batman",
+      s: movieName,
     },
   });
 
+  console.log(movieName);
   console.log(res);
 };
 
-fetchData();
+const inputOneHandler = (event) => {
+  fetchData(event.target.value);
+};
+
+const inputOne = document.getElementById("input-1");
+inputOne.addEventListener("input", inputOneHandler);
